ef704e49dc26efa902107289328c2e3e1f46e1eb
daniel9020 <danyagrblx@gmail.com>
2025-02-12 00:23:39 +0700

    new

--- src/Calculator.java
+++ src/Calculator.java
@@ -35,3 +35,8 @@ public class Calculator {
             System.out.println("Разность чисел " + first + " и " + second + " равна " + resultFour);
             break;
 }}}}
+
+//int maximum = Arrays.stream(array).max().orElse(0);
+//        System.out.println("maximum " + maximum);
+//        int minimum = Arrays.stream(array).min().orElse(0);
+//        System.out.println("minimum " + minimum);
